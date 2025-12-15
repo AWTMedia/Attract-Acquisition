213d86a0f4cc9d53f85473baea87c252d09882bc
AWTMedia <alexander.thomas2401@gmail.com>
2025-12-15 14:27:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import ScrollToTop from "@/components/ScrollToTop";
+
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -18,6 +20,8 @@ const App = () => (
       <Sonner />
 
       <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
