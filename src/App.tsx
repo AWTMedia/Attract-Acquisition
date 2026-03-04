6e8d938293d6a8242dbfed8dedd3e5e8b743ceba
AWTMedia <alexander.thomas2401@gmail.com>
2026-03-04 09:13:29 +0200

    Add Audit and AuditConfirmation routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Audit from "./pages/Audit";
+import AuditConfirmation from "./pages/AuditConfirmation";
+
+
 
 const queryClient = new QueryClient();
 
@@ -18,6 +22,8 @@ const App = () => (
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
+          <Route path="/audit" element={<Audit />} />
+          <Route path="/audit-confirmation" element={<AuditConfirmation />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
